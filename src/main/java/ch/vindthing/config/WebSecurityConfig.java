c98e0a0cb057ae3a1ab2c5be27956a85178c487e
Stefan Nyffenegger <nyfi777@gmail.com>
2020-12-04 17:24:41 +0100

    -websocket fixes (some)

--- src/main/java/ch/vindthing/config/WebSecurityConfig.java
+++ src/main/java/ch/vindthing/config/WebSecurityConfig.java
@@ -57,9 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
                 .antMatchers("/api/app/**").permitAll()
-                .antMatchers("/webjars/**").permitAll()
                 .antMatchers("/rest/**").permitAll()
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
