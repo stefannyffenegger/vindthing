3466bf83188b1f931bf72ea4686b06fa141fc650
Stefan Nyffenegger <nyfi777@gmail.com>
2020-12-21 23:08:08 +0100

    -junit update

--- src/test/java/ch/vindthing/VindThingBackendTest.java
+++ src/test/java/ch/vindthing/VindThingBackendTest.java
@@ -1,17 +1,13 @@
 package ch.vindthing;
 
+import ch.vindthing.model.Item;
 import ch.vindthing.model.Store;
-import ch.vindthing.model.User;
-import ch.vindthing.payload.request.LoginRequest;
-import ch.vindthing.payload.request.ProfileUpdateRequest;
-import ch.vindthing.payload.request.SignupRequest;
-import ch.vindthing.payload.request.StoreAddRequest;
-import ch.vindthing.payload.response.StoreResponse;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import ch.vindthing.payload.request.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -21,14 +17,12 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
 public class VindThingBackendTest {
@@ -39,8 +33,8 @@ public class VindThingBackendTest {
 	int randomServerPort;
 
 	private String token;
-	private String storeId;
-	private String itemId;
+	static String storeId;
+	static String itemId;
 
 	@Before
 	@Test
@@ -111,7 +105,7 @@ public class VindThingBackendTest {
 	}
 
 	@Test
-	public void testStoreAdd() throws URISyntaxException {
+	public void test1StoreAdd() throws URISyntaxException {
 		final String baseUrl = "http://localhost:"+randomServerPort+"/api/store/add";
 		URI uri = new URI(baseUrl);
 		StoreAddRequest storeAddRequest = new StoreAddRequest("Test Bookshelf", "For books",
@@ -123,13 +117,110 @@ public class VindThingBackendTest {
 		HttpEntity<StoreAddRequest> request = new HttpEntity<>(storeAddRequest, headers);
 		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.POST, request, String.class);
 
-		System.out.println(result.getBody());
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Store store = mapper.readValue(result.getBody(), Store.class);
+			storeId = store.getId();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 
 		Assert.assertEquals(201, result.getStatusCodeValue());
+		Assert.assertTrue(result.getBody().contains("Test Bookshelf"));
+		Assert.assertNotNull(storeId);
 	}
 
+	@Test
+	public void test2StoreUpdate() throws URISyntaxException {
+		final String baseUrl = "http://localhost:"+randomServerPort+"/api/store/update";
+		URI uri = new URI(baseUrl);
 
+		StoreUpdateRequest storeUpdateRequest = new StoreUpdateRequest(storeId,"Test Bookshelf Updated",
+				"For books updated", "Living room updated");
 
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+
+		HttpEntity<StoreUpdateRequest> request = new HttpEntity<>(storeUpdateRequest, headers);
+		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.PUT, request, String.class);
+
+		Assert.assertEquals(200, result.getStatusCodeValue());
+		Assert.assertTrue(result.getBody().contains("Test Bookshelf Updated"));
+	}
+
+	@Test
+	public void test2StoreCommentAdd() throws URISyntaxException {
+		final String baseUrl = "http://localhost:"+randomServerPort+"/api/store/comment/add";
+		URI uri = new URI(baseUrl);
+		CommentAddRequest commentAddRequest = new CommentAddRequest("This is a test comment", storeId);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+
+		HttpEntity<CommentAddRequest> request = new HttpEntity<>(commentAddRequest, headers);
+		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.POST, request, String.class);
+
+		Assert.assertEquals(200, result.getStatusCodeValue());
+		Assert.assertTrue(result.getBody().contains("This is a test comment"));
+	}
+
+	@Test
+	public void test2ItemAdd() throws URISyntaxException {
+		final String baseUrl = "http://localhost:"+randomServerPort+"/api/item/add";
+		URI uri = new URI(baseUrl);
+		ItemAddRequest itemAddRequest = new ItemAddRequest("Java for dummies", "Test Item", 2,
+				storeId);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+
+		HttpEntity<ItemAddRequest> request = new HttpEntity<>(itemAddRequest, headers);
+		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.POST, request, String.class);
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Item item = mapper.readValue(result.getBody(), Item.class);
+			itemId = item.getId();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(201, result.getStatusCodeValue());
+		Assert.assertTrue(result.getBody().contains("Java for dummies"));
+	}
+
+	@Test
+	public void test3ItemUpdate() throws URISyntaxException {
+		final String baseUrl = "http://localhost:"+randomServerPort+"/api/item/update";
+		URI uri = new URI(baseUrl);
+		ItemUpdateRequest itemUpdateRequest = new ItemUpdateRequest(itemId, "Java for dummies updated",
+				"Test Item update", 3, false);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+
+		HttpEntity<ItemUpdateRequest> request = new HttpEntity<>(itemUpdateRequest, headers);
+		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.POST, request, String.class);
+
+		Assert.assertEquals(200, result.getStatusCodeValue());
+		Assert.assertTrue(result.getBody().contains("Java for dummies updated"));
+	}
+
+	@Test
+	public void test3ItemUpdateWrongParameters() throws URISyntaxException {
+		final String baseUrl = "http://localhost:"+randomServerPort+"/api/item/update";
+		URI uri = new URI(baseUrl);
+		ItemUpdateRequest itemUpdateRequest = new ItemUpdateRequest("12344klsj3", "Java for dummies updated",
+				"Test Item update", 3, false);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+
+		HttpEntity<ItemUpdateRequest> request = new HttpEntity<>(itemUpdateRequest, headers);
+		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.POST, request, String.class);
+
+		Assert.assertEquals(400, result.getStatusCodeValue());
+	}
 
 	@Test
 	public void testGetAll() throws URISyntaxException {
